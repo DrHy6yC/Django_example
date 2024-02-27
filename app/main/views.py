705faf757ef81791c0845bc660d8a0a113e226bb
Denis Plaksin <dr.hy6yc@gmail.com>
2024-02-27 22:19:51 +0300

    Filling index.html page via views

--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,15 @@ def index(request):
     context = {
         'title': "Bot for English",
         'name_logo': "Bot",
-        'content': "",
+        'category1': "Для учеников",
+        'action1': "Получение результатов тестов",
+        'sub_action1': "А так же получение новых заданий, слов и тестов",
+        'category2': "Для учителя",
+        'action2': "Проверка Д/З",
+        'sub_action2': "Раздача новых домашних заданий, проверка слов и тестов",
+        'category3': "Для будуших студентов",
+        'action3': "Ознакомиться с курсами и ценами",
+        'sub_action3': "Пощупать бота и получить консультацию",
     }
 
     return render(request, "main/index.html", context)

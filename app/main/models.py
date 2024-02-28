cec4aaa648e0828282f916088d069cee23ebe087
Denis Plaksin <dr.hy6yc@gmail.com>
2024-02-28 15:44:05 +0300

    Add test-model USERS and migrate to SQL

--- app/main/models.py
+++ app/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    USER_TG_ID = models.BigIntegerField(unique=True)
+    USER_LOGIN = models.CharField(max_length=200, unique=True)
+    USER_FULL_NAME = models.CharField(max_length=256)
+    USER_LEVEL = models.SmallIntegerField()
+    USER_ACCESS = models.SmallIntegerField()
+    CREATE_TIME = models.DateTimeField()
+    UPDATE_TIME = models.DateTimeField()
+
+    class Meta:
+        db_table = 'USERS'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'

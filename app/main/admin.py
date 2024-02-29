b066b97450322a784837b134d312be3a5c13512b
denisigorevich <dr.hy6yc@gmail.com>
2024-02-29 16:40:20 +0300

    Create Models into main-app, add to admin.py

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from main.models import Users
+from main.models import UserTG, Level, Acces
 
 
-admin.site.register(Users)
+admin.site.register(Level)
+admin.site.register(Acces)
+admin.site.register(UserTG)
--- app/main/models.py
+++ app/main/models.py
@@ -1,15 +1,40 @@
 from django.db import models
 
-class Users(models.Model):
+
+class Acces(models.Model):
+    LEVEL_TEXT = models.CharField(max_length=200, unique=True)
+    CREATE_TIME = models.DateTimeField(auto_now_add=True)
+    UPDATE_TIME = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "Acces"
+        verbose_name = "Доступ"
+        verbose_name_plural = "Доступы"
+
+
+class Level(models.Model):
+    LEVEL_TEXT = models.CharField(max_length=200, unique=True)
+    MIN_LEVEL_SCORE = models.SmallIntegerField()
+    MAX_LEVEL_SCORE = models.SmallIntegerField()
+    CREATE_TIME = models.DateTimeField(auto_now_add=True)
+    UPDATE_TIME = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "LEVELS"
+        verbose_name = "Уровень"
+        verbose_name_plural = "Уровни"
+
+
+class UserTG(models.Model):
     USER_TG_ID = models.BigIntegerField(unique=True)
     USER_LOGIN = models.CharField(max_length=200, unique=True)
     USER_FULL_NAME = models.CharField(max_length=256)
-    USER_LEVEL = models.SmallIntegerField()
-    USER_ACCESS = models.SmallIntegerField()
-    CREATE_TIME = models.DateTimeField()
-    UPDATE_TIME = models.DateTimeField()
+    USER_LEVEL = models.ForeignKey(to=Level, on_delete=models.CASCADE)
+    USER_ACCESS = models.ForeignKey(to=Acces, on_delete=models.CASCADE)
+    CREATE_TIME = models.DateTimeField(auto_now_add=True)
+    UPDATE_TIME = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'USERS'
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        db_table = "USER_TG"
+        verbose_name = "Пользователь Телеграмм"
+        verbose_name_plural = "Пользователи Телеграмм"

fe38ce3e49f85d2a9876871354a6da8633d84556
denisigorevich <dr.hy6yc@gmail.com>
2024-03-01 11:16:34 +0300

    Прописал в моделях наименование полей для отобрадение в админ панели.

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 
-from main.models import User, UserQuize, UserLevel, UserAnswer, QuizeAnswer, \
-QuizeQuestion, QuizeStatus, Constant, QuizeTrueAnswer, Quize, Acces
+from main.models import User, UserQuize, LevelUser, UserAnswer, QuizeAnswer, \
+QuizeQuestion, StatusQuize, Constant, QuizeTrueAnswer, Quize, AccesUser
 
 
 admin.site.register(User)
 admin.site.register(UserQuize)
-admin.site.register(UserLevel)
+admin.site.register(LevelUser)
 admin.site.register(UserAnswer)
 admin.site.register(QuizeAnswer)
 admin.site.register(QuizeQuestion)
-admin.site.register(QuizeStatus)
+admin.site.register(StatusQuize)
 admin.site.register(Constant)
 admin.site.register(QuizeTrueAnswer)
 admin.site.register(Quize)
-admin.site.register(Acces)
+admin.site.register(AccesUser)
--- app/main/models.py
+++ app/main/models.py
@@ -1,23 +1,11 @@
 from django.db import models
 
 
-class Acces(models.Model):
-    level_text = models.CharField(max_length=200, unique=True, blank=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = "Acces"
-        verbose_name = "Доступ"
-        verbose_name_plural = "Доступы"
-
-
 class Constant(models.Model):
-    id = models.AutoField(primary_key=True)
-    constant_name = models.CharField(max_length=200, blank=True)
-    constant_value = models.CharField(max_length=200, blank=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
+    constant_name = models.CharField(max_length=200, blank=True, verbose_name="Наименование переменной")
+    constant_value = models.CharField(max_length=200, blank=True, verbose_name="Значение переменной")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
 
     class Meta:
         db_table = "constant"
@@ -28,43 +16,39 @@ class Constant(models.Model):
         return self.constant_name
 
 
-class QuizeAnswer(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_quize = models.OneToOneField("Quize", models.CASCADE, unique=True, blank=True)
-    question_number = models.IntegerField(blank=True)
-    answer_number = models.IntegerField(blank=True)
-    answer_text = models.CharField(max_length=200, blank=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
+class LevelUser(models.Model):
+    level_text = models.CharField(max_length=200, blank=True, verbose_name="Уровень")
+    min_level_score = models.IntegerField(blank=True, verbose_name="Минималтный процент уровня")
+    max_level_score = models.IntegerField(blank=True, verbose_name="Максимальный процент уровня")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
 
     class Meta:
-        db_table = "quize_answer"
-        verbose_name = "Ответ теста"
-        verbose_name_plural = "Ответы теста"
+        db_table = "level_user"
+        verbose_name = "Уровень пользователя"
+        verbose_name_plural = "Уровни пользователя"
 
 
-class QuizeQuestion(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_quize = models.OneToOneField("Quize", models.CASCADE, unique=True, blank=True)
-    question_number = models.IntegerField(blank=True)
-    question_text = models.CharField(max_length=200, blank=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = "quize_question"
-        verbose_name = "Вопрос теста"
-        verbose_name_plural = "Вопросы теста"
-
-
-class QuizeStatus(models.Model):
-    id = models.AutoField(primary_key=True)
-    status_text = models.CharField(max_length=200, blank=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
+class AccesUser(models.Model):
+    acces_text = models.CharField(
+        max_length=200, unique=True, blank=True, verbose_name="Название доступа"
+    )
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
 
     class Meta:
-        db_table = "quize_qtatus"
+        db_table = "acces_user"
+        verbose_name = "Доступ"
+        verbose_name_plural = "Доступы"
+
+
+class StatusQuize(models.Model):
+    status_text = models.CharField(max_length=200, blank=True, verbose_name="Статус")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    class Meta:
+        db_table = "status_quize"
         verbose_name = "Статус теста"
         verbose_name_plural = "Статусы теста"
 
@@ -72,28 +56,11 @@ class QuizeStatus(models.Model):
         return self.status_text
 
 
-class QuizeTrueAnswer(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_quize = models.OneToOneField("Quize", models.CASCADE, unique=True, blank=True)
-    id_answer = models.OneToOneField(
-        QuizeAnswer, models.CASCADE, unique=True, blank=True
-    )
-    question_number = models.IntegerField(blank=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = "quize_true_answer"
-        verbose_name = "Правильный вопрос для теста"
-        verbose_name_plural = "Правильные вопросы для теста"
-
-
 class Quize(models.Model):
-    id = models.AutoField(primary_key=True)
-    quize_name = models.CharField(max_length=200, blank=True)
-    quize_description = models.CharField(max_length=200, blank=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
+    quize_name = models.CharField(max_length=200, blank=True, verbose_name="Имя теста")
+    quize_description = models.CharField(max_length=200, blank=True, verbose_name="Описание теста")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
 
     class Meta:
         db_table = "quize"
@@ -104,72 +71,101 @@ class Quize(models.Model):
         return self.quize_name
 
 
+class QuizeAnswer(models.Model):
+    id_quize = models.OneToOneField(
+        Quize, models.CASCADE, unique=True, blank=True, verbose_name="ID теста"
+    )
+    question_number = models.IntegerField(blank=True, verbose_name="Номер вопроса")
+    answer_number = models.IntegerField(blank=True, verbose_name="Номер ответа")
+    answer_text = models.CharField(max_length=200, blank=True, verbose_name="Текст ответа")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    class Meta:
+        db_table = "quize_answer"
+        verbose_name = "Ответ теста"
+        verbose_name_plural = "Ответы теста"
+
+
+class QuizeQuestion(models.Model):
+    id_quize = models.OneToOneField(
+        Quize, models.CASCADE, unique=True, blank=True, verbose_name="ID теста"
+    )
+    question_number = models.IntegerField(blank=True, verbose_name="Номер вопроса")
+    question_text = models.CharField(max_length=200, blank=True, verbose_name="Текст вопроса")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    class Meta:
+        db_table = "quize_question"
+        verbose_name = "Вопрос теста"
+        verbose_name_plural = "Вопросы теста"
+
+
+class QuizeTrueAnswer(models.Model):
+    id_quize = models.OneToOneField(
+        Quize, models.CASCADE, unique=True, blank=True, verbose_name="ID теста"
+    )
+    id_answer = models.OneToOneField(
+        QuizeAnswer, models.CASCADE, unique=True, blank=True, verbose_name="ID ответа"
+    )
+    question_number = models.IntegerField(blank=True, verbose_name="Номер вопроса")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    class Meta:
+        db_table = "quize_true_answer"
+        verbose_name = "Правильный вопрос для теста"
+        verbose_name_plural = "Правильные вопросы для теста"
+
+
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_user_tg = models.BigIntegerField(unique=True, blank=True)
-    user_login = models.CharField(max_length=200, blank=True)
-    user_full_name = models.CharField(max_length=200, blank=True)
-    user_level = models.OneToOneField("UserLevel", models.CASCADE, unique=True)
-    user_access = models.OneToOneField("Acces", models.CASCADE, unique=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
+    id_user_tg = models.BigIntegerField(unique=True, blank=True, verbose_name="ID пользователя в телеграм")
+    user_login = models.CharField(max_length=200, blank=True, verbose_name="Лонин пользователя в телеграмм")
+    user_full_name = models.CharField(max_length=200, blank=True, verbose_name="Полное имя и фамилия из телеграмм")
+    user_level = models.OneToOneField(LevelUser, models.CASCADE, unique=True, verbose_name='Уровень пользователя')
+    user_access = models.OneToOneField(AccesUser, models.CASCADE, unique=True, verbose_name='Доступ пользователя')
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
 
     class Meta:
         db_table = "user"
-        verbose_name = "Пользователь в telegramm"
-        verbose_name_plural = "Пользователи в telegramm"
+        verbose_name = "Пользователь в telegram"
+        verbose_name_plural = "Пользователи в telegram"
 
     def __str__(self) -> str:
-        return f'{self.id_user_tg} {self.user_login}'
-
-
-class UserAnswer(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_user_tg = models.OneToOneField(
-        User, models.CASCADE, unique=True, to_field="id_user_tg"
-    )
-    id_user_quize = models.OneToOneField(
-        "UserQuize", models.CASCADE, unique=True, to_field="id"
-    )
-    id_answer = models.OneToOneField(QuizeAnswer, models.CASCADE, unique=True)
-    question_number = models.IntegerField(blank=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = "user_answer"
-        verbose_name = "Ответ пользователя"
-        verbose_name_plural = "Ответы пользователя"
-
-
-class UserLevel(models.Model):
-    id = models.AutoField(primary_key=True)
-    level_text = models.CharField(max_length=200, blank=True)
-    min_level_score = models.IntegerField(blank=True)
-    max_level_score = models.IntegerField(blank=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = "user_level"
-        verbose_name = "Уровень пользователя"
-        verbose_name_plural = "Уровни пользователя"
+        return f"{self.id_user_tg} {self.user_login}"
 
 
 class UserQuize(models.Model):
-    id = models.AutoField(primary_key=True)
     id_user_tg = models.OneToOneField(
-        User, models.CASCADE, unique=True, to_field="id_user_tg"
+        User, models.CASCADE, unique=True, to_field="id_user_tg", verbose_name='ID пользователя в телеграмм'
     )
-    id_quize = models.OneToOneField(Quize, models.CASCADE, unique=True)
-    quize_status = models.OneToOneField(QuizeStatus, models.CASCADE, unique=True)
-    question_number = models.IntegerField(blank=True)
-    id_answer_last = models.IntegerField(blank=True)
-    quize_score = models.IntegerField()
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
+    id_quize = models.OneToOneField(Quize, models.CASCADE, unique=True, verbose_name='ID теста')
+    quize_status = models.OneToOneField(StatusQuize, models.CASCADE, unique=True, verbose_name='ID статуса теста')
+    question_number = models.IntegerField(blank=True, verbose_name="Номер вопроса")
+    id_answer_last = models.IntegerField(blank=True, verbose_name="ID последнего ответа пользователя")
+    quize_score = models.IntegerField(verbose_name='Количество баллов пользователя')
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
 
     class Meta:
         db_table = "user_quize"
         verbose_name = "Запущенный тест пользователя"
         verbose_name_plural = "Запущенные тесты пользователя"
+
+
+class UserAnswer(models.Model):
+    id_user_tg = models.OneToOneField(
+        User, models.CASCADE, unique=True, to_field="id_user_tg", verbose_name='ID пользователя в телеграм'
+    )
+    id_user_quize = models.OneToOneField(UserQuize, models.CASCADE, unique=True, verbose_name='ID запушеного пользователем теста')
+    id_answer = models.OneToOneField(QuizeAnswer, models.CASCADE, unique=True, verbose_name='ID ответа')
+    question_number = models.IntegerField(blank=True, verbose_name="Номер вопроса")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    class Meta:
+        db_table = "user_answer"
+        verbose_name = "Ответ пользователя"
+        verbose_name_plural = "Ответы пользователя"
